f6be3fe99ed07dab8b31eebd5dd6ba5bca753688
Sanches Santos <sanchesfranklin@hotmail.com>
2023-03-26 00:06:03 -0300

    first deploy

---
+++ api/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+migrate = Migrate(app, db)
+
+
+api = Api(app)
+
+from .views import tarefa_views
+from .models import tarefa_model
---
+++ api/views/tarefa_views.py
@@ -0,0 +1,73 @@
+from flask_restful import Resource
+from api import api
+from ..schema import tarefa_schema
+from flask import request, make_response, jsonify
+from ..entidades import tarefa
+from ..services import tarefa_service
+
+class TarefaList(Resource):
+    # Listar todas as Tarefas
+    def get(self):
+        tarefas = tarefa_service.listar_tarefas()
+        ts = tarefa_schema.TarefaSchema(many=True)
+        return make_response(ts.jsonify(tarefas), 200)
+    
+    # Cadastrar Tarefa
+    def post(self):
+        ts = tarefa_schema.TarefaSchema()
+        validate = ts.validate(request.json)
+        if validate:
+            return make_response(jsonify(validate), 400)
+        else:
+            titulo = request.json["titulo"]
+            descricao = request.json["descricao"]
+            data_expiracao = request.json["data_expiracao"]
+            tarefa_nova = tarefa.Tarefa(titulo=titulo, descricao=descricao, data_expiracao=data_expiracao)
+            result = tarefa_service.cadastrar_tarefa(tarefa_nova)
+            return make_response(ts.jsonify(result), 201)
+
+class TarefaDetail(Resource):
+    
+    # Listar Tarefa por ID
+    def get(self, id):
+        tarefa = tarefa_service.listar_tarefa_id(id)
+        if tarefa is None:
+            return make_response(jsonify("Tarefa não encontrada"), 404)
+        
+        ts = tarefa_schema.TarefaSchema()
+        return make_response(ts.jsonify(tarefa), 200)
+            
+    
+    # Editar Tarefa
+    def put(self, id):
+        tarefa_bd = tarefa_service.listar_tarefa_id(id)
+        if tarefa_bd is None:
+            return make_response(jsonify("Tarefa não encontrada"), 404)
+        
+        ts = tarefa_schema.TarefaSchema()
+        validate = ts.validate(request.json)
+        if validate:
+            return make_response(jsonify(validate), 400)
+        else:
+            titulo = request.json["titulo"]
+            descricao = request.json["descricao"]
+            data_expiracao = request.json["data_expiracao"]
+            tarefa_nova = tarefa.Tarefa(titulo=titulo, descricao=descricao,data_expiracao=data_expiracao)
+            tarefa_service.editar_tarefa(tarefa_bd, tarefa_nova)
+            tarefa_atualizada = tarefa_service.listar_tarefa_id(id)
+            return make_response(ts.jsonify(tarefa_atualizada), 200)
+    
+    # Remover Tarefa
+    def delete(self, id):
+        tarefa = tarefa_service.listar_tarefa_id(id)
+        if tarefa is None:
+            return make_response(jsonify("Tarefa não encontrada"), 404)
+        tarefa_service.remover_tarefa(tarefa)
+        return make_response('', 204)
+    
+    
+    
+    
+    
+api.add_resource(TarefaList, '/tarefas')
+api.add_resource(TarefaDetail, '/tarefas/<int:id>')
